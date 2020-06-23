19ff5bb8ce1b9d418f182da8fd6bfcf07dfbb303
j0thu <jonathanben215@gmail.com>
2020-06-23 22:57:15 +0530

    NEW

--- server.js
+++ server.js
@@ -5,6 +5,8 @@ if(process.env.NODE_ENV !== 'production'){
 const express = require('express');
 const app = express();
 const expressLayouts = require('express-ejs-layouts');
+const mongoose = require('mongoose');
+
 
 const indexRouter = require('./routes/index');
 
@@ -15,8 +17,16 @@ app.set('layout', 'layouts/layout');
 app.use(expressLayouts);
 app.use(express.static('public'));
 
-const mongoose = require('mongoose');
-mongoose.connect(process.env.DATABASE_URL, {useNewUrlParser: true, useUnifiedTopology:true})
+mongoose.connect(process.env.DATABASE_URL, 
+    {
+        dbName: process.env.DB_NAME,
+        user: process.env.DB_USER,
+        pass: process.env.DB_PASS,
+        useNewUrlParser: true,
+        useUnifiedTopology: true,   
+    });
+
+
 const db = mongoose.connection;
 db.on('error', error => console.error(error));
 db.once('open', () => console.log('Connected to Mongoose')); //Connecting and opening the database for the first time 
