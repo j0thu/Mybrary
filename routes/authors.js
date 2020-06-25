9acc251d14b75e6d2e36b3e83b75f1398eb65d47
j0thu <jonathanben215@gmail.com>
2020-06-25 20:06:32 +0530

    initial author routes

---
+++ routes/authors.js
@@ -0,0 +1,46 @@
+const express = require('express');
+const router = express.Router();
+
+const Author = require('../models/author');  //Importing the author model to use 
+
+//All authors route
+router.get('/', async(req, res)=> {
+    let searchOptions = {}
+    if(req.query.name != null && req.query.name !== ''){
+        searchOptions.name = new RegExp(req.query.name, 'i') //i flag means, that it is case insensitive
+    }
+    try{
+        const authors = await Author.find(searchOptions);
+        res.render('authors/index', {authors:authors, searchOptions: req.query});
+    }
+    catch{
+        res.redirect('/');
+
+    }
+})
+
+//New Author Route
+router.get('/new', (req, res)=> {
+    res.render('authors/new', {author: new Author()});  // For using the author model in the ejs file
+})
+
+//Creating a new author
+router.post('/', async(req, res)=> {
+    const author = new Author({
+        name: req.body.name
+    })
+    try{
+        const newAuthor = await author.save(); //waits for author.save to finish
+        // res.redirect(`authors/${newAuthor.id}`)
+        res.redirect('authors');
+    }
+    catch{
+            res.render('authors/new', {
+            author: author,
+            errorMessage: 'Error creating Author'
+     })
+    }
+})
+
+module.exports = router;
+
--- server.js
+++ server.js
@@ -5,10 +5,11 @@ if(process.env.NODE_ENV !== 'production'){
 const express = require('express');
 const app = express();
 const expressLayouts = require('express-ejs-layouts');
-const mongoose = require('mongoose');
-
+const bodyParser = require('body-parser');
 
 const indexRouter = require('./routes/index');
+const authorRouter = require('./routes/authors');
+
 
 app.set('view engine', 'ejs');
 app.set('views', __dirname + '/views'); //All the views are coming from this directory
@@ -16,7 +17,9 @@ app.set('layout', 'layouts/layout');
 
 app.use(expressLayouts);
 app.use(express.static('public'));
+app.use(bodyParser.urlencoded({limit: '10mb', extended: false}));
 
+const mongoose = require('mongoose');
 mongoose.connect(process.env.DATABASE_URL, 
     {
         dbName: process.env.DB_NAME,
@@ -34,6 +37,8 @@ db.once('open', () => console.log('Connected to Mongoose')); //Connecting and op
 
 
 app.use('/', indexRouter);
+app.use('/authors', authorRouter);
 
 
 app.listen(process.env.PORT || 3000);
+ 
